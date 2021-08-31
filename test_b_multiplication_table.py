c5a70cc48cebfad7a76ad4a0dcacf2efcfa94259
Eric Orso <eric.orso@arol.com>
2021-08-31 10:11:17 +0200

    square multiplication table

--- test_b_multiplication_table.py
+++ test_b_multiplication_table.py
@@ -1,10 +1,35 @@
 ##  @package multiplication table
 #ask for a number
 #print the multiplication table for theat number
+#square
 
 #ask user for number
-user_num = input("Gimme NUMBER!")
-#scan mul factor
-for cnt in range(1, 10+1):
-    #print the multiplication table
-    print(f"{int(user_num)}\t*\t{int(cnt)}\t= {int(user_num) *int(cnt)}")
+user_str = input('Gimme NUMBER!')
+user_num = int(user_str)
+assert(user_num >0), f"... DVMMY!"
+#activate header
+u1_header = True
+#scan mul factors on rows
+for num_row in range(1, user_num+1 +1):
+
+    #if print header mode
+    if u1_header == True:
+        print(f" \t",end='')
+    #if print row header
+    else:
+        #print row header
+        print(f"{int(num_row-1)}\t",end='')
+    
+    #scan mul factors on cols
+    for num_col in range(1, user_num+1):
+        if u1_header == True:
+            print(f"| {int(num_col)}\t",end='')
+        else:
+            #print row header
+            print(f"| {int(num_row-1) *int(num_col)}\t",end='')
+            
+    #deactivate header mode
+    u1_header = False
+
+    #end row
+    print("\n")
