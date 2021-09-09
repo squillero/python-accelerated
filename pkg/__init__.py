dc53d83da160ac19871066d3e088eb9c95f0eed0
Giovanni Squillero <giovanni.squillero@polito.it>
2021-09-09 10:22:18 +0200

    Final clean-up

--- pkg/__init__.py
+++
@@ -1,9 +0,0 @@
-# Example program from https://github.com/squillero/python-accelerated
-# Copyright © 2021 Giovanni Squillero <squillero@polito.it>
-# Free for personal or classroom use; see 'LICENCE.md' for details.
-
-"""
-Commento!
-"""
-
-from .file_c import foo
--- pkg/file_c.py
+++
@@ -1,7 +0,0 @@
-# Example program from https://github.com/squillero/python-accelerated
-# Copyright © 2021 Giovanni Squillero <squillero@polito.it>
-# Free for personal or classroom use; see 'LICENCE.md' for details.
-
-
-def foo():
-    print("tkj hkd hdkdg fdk,fb kfglbdfkg, foo db file_c")
